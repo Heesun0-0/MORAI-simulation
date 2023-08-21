30c9db5ce5306ae3ebe8bb7b103e2ef0fde26c1c
Heesun0-0 <limhs727@naver.com>
2023-08-21 16:05:01 +0900

    slam

---
+++ slam/src/kw_tf/scripts/application.py
@@ -0,0 +1,30 @@
+import rospy
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+import actionlib
+
+rospy.init_node('application', anonymous =True)
+client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
+client.wait_for_server()
+
+while(True):
+    goal = MoveBaseGoal()
+    goal.target_pose.header.frame_id = "map"
+    goal.target_pose.header.stamp = rospy.Time.now()
+    goal.target_pose.pose.position.x = 1.0
+    goal.target_pose.pose.orientation.w = 0.906
+    goal.target_pose.pose.orientation.z = -0.423
+    
+    client.send_goal(goal)
+    wait = client.wait_for_result()
+    
+    
+    goal = MoveBaseGoal()
+    goal.target_pose.header.frame_id = "map"
+    goal.target_pose.header.stamp = rospy.Time.now()
+    goal.target_pose.pose.position.x = 20.0
+    goal.target_pose.pose.position.y = 20.0
+    goal.target_pose.pose.orientation.w = 0.906
+    goal.target_pose.pose.orientation.z = -0.423
+    
+    client.send_goal(goal)
+    wait = client.wait_for_result()
